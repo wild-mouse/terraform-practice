c77fe876810b3a7995eb049c9ff3f3eef91f969e
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-03-01 16:24:45 +0900

    Add security group

--- example/main.tf
+++ example/main.tf
@@ -17,13 +17,36 @@ data "aws_ami" "recent_amazon_liux_2" {
   }
 }
 
+resource "aws_security_group" "example_ec2" {
+  name = "example-ec2"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "example" {
   ami = data.aws_ami.recent_amazon_liux_2.image_id
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.example_ec2.id]
 
   user_data = <<EOF
     #!/bin/bash
     yum install -y httpd
     systemctl start httpd.service
 EOF
+}
+
+output "example_public_dns" {
+  value = aws_instance.example.public_dns
 }
