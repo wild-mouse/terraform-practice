bbf9039b53455b25e476f87731be5c41f977fa61
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-03-01 17:09:17 +0900

    Add using module

--- example/main.tf
+++ example/main.tf
@@ -2,47 +2,11 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-data "aws_ami" "recent_amazon_liux_2" {
-  most_recent = true
-  owners = ["amazon"]
-
-  filter {
-    name = "name"
-    values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
-  }
-
-  filter {
-    name = "state"
-    values = ["available"]
-  }
-}
-
-resource "aws_security_group" "example_ec2" {
-  name = "example-ec2"
-
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "example" {
-  ami = data.aws_ami.recent_amazon_liux_2.image_id
+module "web_server" {
+  source = "./http_server"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.example_ec2.id]
-
-  user_data = file("./user_data.sh")
 }
 
-output "example_public_dns" {
-  value = aws_instance.example.public_dns
+output "public_dns" {
+  value = module.web_server.public_dns
 }
